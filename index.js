a44e79b8afa2e84da4253967e247ddc1454934de
Alexander Obi <alexanderobi#@gmail.com>
2024-03-06 18:40:10 +0100

    Changed the host to localhost

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const http = require("http");
+const { hostname } = require("os");
 const port = 8900;
+host_name = "localhost";
 
 const server = http.createServer((req, res) => {
   res.end("Ohayo. I am Obi Chidubem Michael.");
 });
 
-server.listen(port, () => {
+server.listen(port, host_name, () => {
   console.log(`Server running successfully on ${port}`);
 });
